9ccc3b3934596e7a17b9463d971fbc896f062b10
deapthspin <thedeapthspin@gmail.com>
2024-01-18 18:56:03 +0800

    test

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "./https://deapthspin.github.io/chinese-orientation-thing"
+  base: "./deapthspin.github.io/chinese-orientation-thing"
 })
